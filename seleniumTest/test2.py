c05765348778f24b6ed49a016ea95092b755fe15
huangzhif <394320736@qq.com>
2018-08-09 16:33:31 +0800

    浏览器尺寸，前进及后退

---
+++ seleniumTest/test2.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+from selenium import webdriver
+import time
+
+browser = webdriver.Firefox()
+url = "http://www.baidu.com"
+
+browser.get(url)
+
+browser.find_element_by_id("kw").send_keys("selenium")
+browser.find_element_by_id("su").click()
+
+
+time.sleep(3)
+browser.back() #返回
+time.sleep(2)
+browser.forward() #前进
+#browser.minimize_window() #最小化窗口
+#time.sleep(2)
+#browser.maximize_window() #最大化窗口
+#browser.set_window_size(400,200) #设置浏览器宽高
+
+time.sleep(2)
+browser.close()
